819e4b9b8ea8a533e78a8bf276747e1725180ac0
Le Quang Tri <quangtri2002lk@gmail.com>
2024-05-21 11:13:47 +0700

    first commit

---
+++ src/constants/auth.ts
@@ -0,0 +1,7 @@
+export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
+const customAPIEndpoint =
+  import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'http://10.1.1.84:8000/v1/chat/completions';
+export const defaultAPIEndpoint =
+  import.meta.env.VITE_DEFAULT_API_ENDPOINT || officialAPIEndpoint;
+
+export const availableEndpoints = [officialAPIEndpoint, customAPIEndpoint];
